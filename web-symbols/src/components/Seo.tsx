a462ca7af37ae9ead765305bf39a606093048aed
Luiz Claudio <luiz.deadzone@gmail.com>
2021-03-27 20:48:24 -0300

    Image Update

--- web-symbols/src/components/Seo.tsx
+++ web-symbols/src/components/Seo.tsx
@@ -9,7 +9,7 @@ export function Seo(){
                 {/*Open Graph*/}
                 <meta property="og:type" content="website"/>
                 <meta property="og:title" content="Web Symbols Code"/>
-                <meta data-react-helmet="true" property="og:image" content="img/WSCwhite.png"/>
+                <meta data-react-helmet="true" property="og:image" content="https://github.com/DinowSauron/Web-Symbols-Code/tree/main/web-symbols/public/img/WSCwhite"/>
 
                 <meta property="og:description" content="Find the most used symbols easily with web symbols, find the symbol, copy your code, favorite your symbol, create personalized folders, everything to make your work easier!"/>
 
