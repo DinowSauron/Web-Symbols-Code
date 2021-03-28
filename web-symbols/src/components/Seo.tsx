c5d0ebb38d61987c5a42cb3a83f0dbcb25dcd093
Luiz Claudio <luiz.deadzone@gmail.com>
2021-03-28 13:24:16 -0300

    Site Verification

--- web-symbols/src/components/Seo.tsx
+++ web-symbols/src/components/Seo.tsx
@@ -6,10 +6,14 @@ export function Seo(){
         <div>
             <h3>hello</h3>
             <Head>
+                <meta name="google-site-verification" content="Z-UjI3t9GdfNDXgiH3K-rd3B3prJrkZ3TLm0M1e1xVM" />
+
                 {/*Open Graph*/}
                 <meta property="og:type" content="website"/>
                 <meta property="og:title" content="Web Symbols Code"/>
                 <meta data-react-helmet="true" property="og:image" content="https://web-symbols-code.vercel.app/img/WSCwhite.png"/>
+
+                <meta property="twitter:card" content="summary_large_image"></meta>
                 <meta data-react-helmet="true" property="twitter:image" content="https://web-symbols-code.vercel.app/img/WSCwhite.png"/>
 
                 <meta property="og:description" content="Find the most used symbols easily with web symbols, find the symbol, copy your code, favorite your symbol, create personalized folders, everything to make your work easier!"/>
